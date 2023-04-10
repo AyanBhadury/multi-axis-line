a75c64c7f76197c8ceb956756f3ff7b3ca49eb5c
AyanBhadury <ayon.bony@gmail.com>
2023-04-10 16:39:29 +0530

    1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, PowerCharts, FusionTheme);
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
